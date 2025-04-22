4ffe82e688fcc6cba5ebb3317a1eac8aa4f3fe28
MaadehahAsif <127966145+MaadehahAsif@users.noreply.github.com>
2025-04-22 20:26:00 +0500

    Update datatypes.py

--- Python/1) Python Basics/Notes/datatypes.py
+++ Python/1) Python Basics/Notes/datatypes.py
@@ -1,1 +1,14 @@
-c
+#There is no way to declare a variable in python. The variable gets declared on assigning a value.
+
+datatypes="""
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+""";
+
+print(datatypes);
